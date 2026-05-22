ab3d0ff1139d156ee9ae89121bc6be2d6c13caa7
Joe Workman <joe@workmanmail.com>
2026-05-22 07:02:42 -0700

    bump to patchy v0.1.1

--- Formula/patchy.rb
+++ Formula/patchy.rb
@@ -1,8 +1,8 @@
 class Patchy < Formula
   desc "Apache + mod_php dev server for macOS"
   homepage "https://github.com/joeworkman/patchy"
-  url "https://github.com/joeworkman/patchy/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7a8d9f2f7f59e79de41f242e123e26f7d14e232ca1635947616e2ac52284ecb4"
+  url "https://github.com/joeworkman/patchy/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "b086d3f72e3ba659e3b98cdc39678cac32ef9d212e644ad90af3698a3f7dfe9d"
   license "MIT"
 
   depends_on "hostess"
